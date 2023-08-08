275488518674265fb4188b26ad6b67bd75e7649c
Jonathan Jacobs <jonathan.jacobs1997@gmail.com>
2023-08-08 16:06:00 +0200

    Add edgy.nvim and remove window-picker

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -677,41 +677,41 @@ return {
 	},
 	{ "nvimdev/lspsaga.nvim" },
 	--{ "kkharji/sqlite.lua", module = "sqlite" },
-	{
-		"s1n7ax/nvim-window-picker",
-		name = "window-picker",
-		event = "VeryLazy",
-		version = "2.*",
-		config = function()
-			require("window-picker").setup({
-				hint = "floating-big-letter",
-				-- hint = 'statusline-winbar',
-				--
-				-- following filters are only applied when you are using the default filter
-				-- defined by this plugin. If you pass in a function to "filter_func"
-				-- property, you are on your own
-				filter_rules = {
-					-- when there is only one window available to pick from, use that window
-					-- without prompting the user to select
-					autoselect_one = true,
-
-					-- whether you want to include the window you are currently on to window
-					-- selection or not
-					include_current_win = true,
-
-					-- filter using buffer options
-					bo = {
-						-- if the file type is one of following, the window will be ignored
-						-- filetype = { 'NvimTree', 'neo-tree', 'notify' },
-
-						filetype = { "notify" },
-						-- if the file type is one of following, the window will be ignored
-						-- buftype = { 'terminal' },
-					},
-				},
-			})
-		end,
-	},
+	-- {
+	-- 	"s1n7ax/nvim-window-picker",
+	-- 	name = "window-picker",
+	-- 	event = "VeryLazy",
+	-- 	version = "2.*",
+	-- 	config = function()
+	-- 		require("window-picker").setup({
+	-- 			hint = "floating-big-letter",
+	-- 			-- hint = "statusline-winbar",
+	-- 			--
+	-- 			-- following filters are only applied when you are using the default filter
+	-- 			-- defined by this plugin. If you pass in a function to "filter_func"
+	-- 			-- property, you are on your own
+	-- 			filter_rules = {
+	-- 				-- when there is only one window available to pick from, use that window
+	-- 				-- without prompting the user to select
+	-- 				autoselect_one = true,
+	--
+	-- 				-- whether you want to include the window you are currently on to window
+	-- 				-- selection or not
+	-- 				include_current_win = true,
+	--
+	-- 				-- filter using buffer options
+	-- 				bo = {
+	-- 					-- if the file type is one of following, the window will be ignored
+	-- 					-- filetype = { 'NvimTree', 'neo-tree', 'notify' },
+	--
+	-- 					filetype = { "notify", "edgy" },
+	-- 					-- if the file type is one of following, the window will be ignored
+	-- 					-- buftype = { 'terminal' },
+	-- 				},
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.2",
