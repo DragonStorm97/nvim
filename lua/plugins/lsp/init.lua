6fcd041ffa3fe8cdcec11a34c549261765260d6c
Jonathan Jacobs <jonathan.jacobs1997@gmail.com>
2024-01-14 18:29:52 +0100

    merge second mason delcaration into first

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -3,7 +3,19 @@ local AddKeymaps = require("plugins.lsp.lsp-utils").AddKeymaps
 return {
 	{
 		"williamboman/mason.nvim",
+		build = ":MasonUpdate",
 		opts = {
+			pip = {
+				upgrade_pip = true,
+			},
+			ui = {
+				border = "rounded",
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
 			ensure_installed = {
 				"stylua",
 				"shellcheck",
@@ -29,6 +41,47 @@ return {
 				"rust-analyzer",
 			},
 		},
+		config = function(_, opts)
+			require("mason").setup(opts)
+			local mr = require("mason-registry")
+			local packages = {
+				"bash-language-server",
+				"black",
+				"clang-format",
+				"clangd",
+				"codelldb",
+				"cspell",
+				"css-lsp",
+				"eslint-lsp",
+				"graphql-language-service-cli",
+				"html-lsp",
+				"json-lsp",
+				"lua-language-server",
+				"markdownlint",
+				"prettier",
+				"pyright",
+				"shfmt",
+				"tailwindcss-language-server",
+				"taplo",
+				"typescript-language-server",
+				"yaml-language-server",
+				"gopls",
+				"editorconfig-checker",
+			}
+			local function ensure_installed()
+				for _, package in ipairs(packages) do
+					local p = mr.get_package(package)
+					if not p:is_installed() then
+						p:install()
+					end
+				end
+			end
+			if mr.refresh then
+				mr.refresh(ensure_installed)
+			else
+				ensure_installed()
+			end
+		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
@@ -747,64 +800,6 @@ return {
 					})
 				end,
 			})
-		end,
-	},
-	{
-		"williamboman/mason.nvim",
-		build = ":MasonUpdate",
-		opts = {
-			pip = {
-				upgrade_pip = true,
-			},
-			ui = {
-				border = "rounded",
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		},
-		config = function(_, opts)
-			require("mason").setup(opts)
-			local mr = require("mason-registry")
-			local packages = {
-				"bash-language-server",
-				"black",
-				"clang-format",
-				"clangd",
-				"codelldb",
-				"cspell",
-				"css-lsp",
-				"eslint-lsp",
-				"graphql-language-service-cli",
-				"html-lsp",
-				"json-lsp",
-				"lua-language-server",
-				"markdownlint",
-				"prettier",
-				"pyright",
-				"shfmt",
-				"tailwindcss-language-server",
-				"taplo",
-				"typescript-language-server",
-				"yaml-language-server",
-				"gopls",
-				"editorconfig-checker",
-			}
-			local function ensure_installed()
-				for _, package in ipairs(packages) do
-					local p = mr.get_package(package)
-					if not p:is_installed() then
-						p:install()
-					end
-				end
-			end
-			if mr.refresh then
-				mr.refresh(ensure_installed)
-			else
-				ensure_installed()
-			end
 		end,
 	},
 }
