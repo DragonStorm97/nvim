ad89b3f9c03de80f54f52c766b2af7b8fde7a5a4
Jonathan Jacobs <jonathan.jacobs1997@gmail.com>
2024-05-26 13:37:20 +0200

    Add mini-surround to lazy extras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,6 +30,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		-- { import = "lazyvim.plugins.extras.lsp.none-ls" },
 		{ import = "lazyvim.plugins.extras.editor.mini-diff" },
+		{ import = "lazyvim.plugins.extras.coding.mini-surround" },
 		-- { import = "lazyvim.plugins.extras.ui.mini-animate" },
 
 		{
