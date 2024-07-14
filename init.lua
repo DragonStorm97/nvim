b8452f8ec35a152f47a39d5b37ba3a8dc9be9829
Johnathan Jacobs <jonathan.jacobs1997@gmail.com>
2024-07-14 17:06:02 +0200

    add wslclipboard and remove neodev as per lazy

--- init.lua
+++ init.lua
@@ -3,3 +3,19 @@
 require("config.lazy")
 require("config.keymaps")
 -- require("core.plugin_config")
+
+-- from https://www.reddit.com/r/neovim/comments/1e1rtct/comment/lcxp5qu/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+if vim.fn.has("wsl") == 1 then
+	vim.g.clipboard = {
+		name = "WslClipboard",
+		copy = {
+			["+"] = "/mnt/c/Windows/System32/clip.exe",
+			["*"] = "/mnt/c/Windows/System32/clip.exe",
+		},
+		paste = {
+			["+"] = '/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+			["*"] = '/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+		},
+		cache_enabled = 0,
+	}
+end
